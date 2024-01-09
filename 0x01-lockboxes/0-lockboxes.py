c3c5265832c1c50c9ea2512d49879a0b7f16110e
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-01-09 22:06:52 +0000

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -38,7 +38,7 @@ def checkKey(boxes, y, boxState):
 
 def canUnlockAll(boxes):
     """ function to find key for a box """
-    if not boxes or len(boxes) < 1:
+    if not boxes:
         return False
     boxState = checkKey(boxes, 0, [])
     boxState[0] = True
