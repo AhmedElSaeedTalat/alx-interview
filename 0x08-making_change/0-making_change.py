a2349d1d8a8e7fb2ac860ba7718858b569af76c0
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-03-02 18:56:35 +0000

    gready apprach used

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,42 +6,14 @@ def makeChange(coins, total):
     """ makeChange function """
     if total <= 0:
         return 0
-
-    sorted_coins = sorted(coins)
-    processed_list = []
-    i = 0
-    while i < len(coins):
-        j = 0
-        row = []
-        while j <= total:
-            if j == 0:
-                value = 0
-                row.append(value)
-                processed_list.append(row)
-                row = []
-            else:
-                if i == 0:
-                    if sorted_coins[i] > j:
-                        value = 0
-                    else:
-                        if j % sorted_coins[i] == 0:
-                            value = int(j / sorted_coins[i])
-                        else:
-                            value = 0
-                    processed_list[i].append(value)
-                else:
-                    if sorted_coins[i] > j:
-                        value = processed_list[i - 1][j]
-                        processed_list[i].append(value)
-                    else:
-                        index = j - sorted_coins[i]
-                        prev_value = processed_list[i - 1][j]
-                        value = min(prev_value, 1 + processed_list[i][index])
-                        processed_list[i].append(value)
-            j += 1
-        i += 1
-    min_coins = processed_list[-1][-1]
-    if min_coins == 0:
-        return -1
-    else:
-        return min_coins
+    sort_coins = sorted(coins)
+    used_coins = []
+    i = len(coins) - 1
+    while i > 0:
+        while total >= sort_coins[i]:
+            total -= sort_coins[i]
+            used_coins.append(sort_coins[i])
+        i -= 1
+    if total == 0:
+        return len(used_coins)
+    return -1
