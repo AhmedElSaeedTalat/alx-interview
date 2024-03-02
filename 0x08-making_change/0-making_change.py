8304ea4dea59247f163d31e46a26de1af26a5a7c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-03-02 20:32:26 +0000

    gready apprach used new try

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,12 +8,16 @@ def makeChange(coins, total):
         return 0
     sort_coins = sorted(coins)
     used_coins = []
-    i = len(coins) - 1
-    while i > 0:
-        while total >= sort_coins[i]:
-            total -= sort_coins[i]
-            used_coins.append(sort_coins[i])
-        i -= 1
+    i = 0
+    while i < len(coins) and total >= 0:
+        y = len(coins) - 1
+        while y >= 0:
+            while total >= sort_coins[y]:
+                total -= sort_coins[y]
+                used_coins.append(sort_coins[y])
+            y -= 1
+        i += 1
+
     if total == 0:
         return len(used_coins)
     return -1
